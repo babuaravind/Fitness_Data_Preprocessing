092f723127d8d8608995e056f9a7dd2854a00583
babuaravind <itsbabuaravind@gmail.com>
2020-03-29 03:28:35 +0530

    Works as intended.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+\#Peer-graded Assignment: Getting and Cleaning Data Course Project
 
+This project/repository contains instructions as well as the source code
+to achieve the Tidy dataset for the human\_activity\_recognition.
+
+\#\#RAW
+[Dataset](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+Files:
+
+\#\#\#run\_analysis.R This file contains the necessary source code to
+process the raw dataset to the finished tidy dataset. It performs the
+following instructions on the raw datset.
+
+1.  Merges the training and the test sets to create one data set.
+2.  Extracts only the measurements on the mean and standard deviation
+    for each measurement.
+3.  Uses descriptive activity names to name the activities in the data
+    set
+4.  Appropriately labels the data set with descriptive variable names.
+5.  Creates a second, independent tidy data set with the average of each
+    variable for each activity and each subject.
+
+\#\#\#CookBook.md Contains documents each step of the data cleaning and
+preprocessing process.
+
+\#\#\#tidy-data-final.txt This is the actual file that has been
+generated after following the aforementioned steps.
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,53 @@
-#init
+# Required Libraries and Packages.
+library(dplyr)
+resource_zip <-"getdata_projectfiles_UCI HAR Dataset.zip"
+# Downloading the course dataset
+if(!file.exists(resource_zip)) {
+  data_source <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(data_source, resource_zip, method='curl')
+}
+
+# Decompressing the zip file.
+if(!file.exists("UCI HAR Dataset")) {
+  unzip(resource_zip)
+}
+
+# Creating data frames
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+X_test <- read.table("UCI HAR Dataset/test/x_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+# Binding the testing and training dataset via the rbind function.
+X <- rbind(X_train, X_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+binded_data <- cbind(Subject, Y, X)
+
+tidy_data <- binded_data %>% select(subject, code, contains("mean"), contains("std"))
+tidy_data$code <- activity_labels[tidy_data$code, 2]
+
+names(tidy_data)[2] = "activity"
+names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
+names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
+names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
+names(tidy_data)<-gsub("-mean()", "Mean", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-std()", "STD", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-freq()", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
+names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+
+dataset <- tidy_data %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(dataset, "tidy-data-final", row.name=FALSE)
+
+str(dataset)
